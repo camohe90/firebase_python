0745ca87d1771916f20d641332b1c102f1cab678
Camilo <camohe90@gmail.com>
2020-10-03 15:45:49 -0500

    Create childs in firebases and get data

--- firebase.py
+++ firebase.py
@@ -3,34 +3,32 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 from time import sleep
-
+datos =[]
 
 
 # Fetch the service account key JSON file contents
-cred = credentials.Certificate('C:/Users/Camilo/haiko/proyecto/clave/clave.json')
+cred = credentials.Certificate('C:/Users/Camilo/haiko/proyecto_BD_2/clave/clave.json')
 # Initialize the app with a service account, granting admin privileges
 firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://haiko-bd.firebaseio.com/'
+    'databaseURL': 'https://fir-haiko.firebaseio.com//'
 })
 
-for i in range (0,100001,10000):
 
-    ref = db.reference("cuentas/camilo")
-    ref.update({
-                    'cuenta1': i,
-                    'cuenta2': i+5000 
-            }) 
-    print("Agregue ", i , "pesos ")              
-    sleep(1.5)   
+for i in range (10):
 
-cont = db.reference('cuentas/camilo/cuenta2').get()
-print("El saldo final de Camilo es ", cont)
+    ref = db.reference("mensajes")
+    mensaje= "herrera" +str(i) 
+    ref.child("3").push(mensaje)
+
+datos = ref.get("mensajes/1")
+print(datos[0])
 
 
 
 
 
     
+
 
 
     
